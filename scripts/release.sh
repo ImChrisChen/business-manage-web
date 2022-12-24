20410d2efba1b9a43f60c8aaf342ad96497a57be
imchrisorz <imchrisorz@gmail.com>
2022-12-24 16:00:24 +0800

    ci: ci change and push

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-source "./scripts/update_version_push.sh"
+#source "./scripts/update_version_push.sh"
 
 image_name="imchrisorz/business-manage-web"
+
+ver=$(cat ./package.json | grep -e '"version":' | tr -cd "[0-9].")
 
 docker build -t "${image_name}:${ver}" .
 
